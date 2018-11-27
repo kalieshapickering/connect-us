6a8fca74af14424d8af14a998a3d0345206f957e
kalistan <kaliesha.pickering@gmail.com>
2018-11-26 21:06:27 -0500

    scrape from elle.com complete

---
+++ routes/api/articles.js
@@ -0,0 +1,16 @@
+// const router = require("express").Router();
+// const articleController = require("../../controllers/articleController");
+// const commentController = require("../../controllers/commentController");
+
+// router.route("/")
+// .get(articleController.findAll)
+// .post(commentController.create);
+
+// router
+// .route("/:id")
+// .get(articleController.findById)
+// .get(commentController.findById)
+// .put(commentController.update)
+// .delete(commentController.remove);
+
+// module.export = router;
